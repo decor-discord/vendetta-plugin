0bc701baa786eaf76c2314e6f3352e32490586cf
Jack Matthews <jm5112356@gmail.com>
2023-04-15 05:11:58 -0400

    Fetch users after patches

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,12 @@ const ImageResolver = findByProps("getAvatarDecorationURL", "default");
 
 const BASE_URL = "https://decor.fieryflames.dev";
 
-let users;
+let users: Set<string>;
 
 let patches = [];
 
 export default {
     onLoad: async () => {
-        users = new Set(await fetch(BASE_URL + "/users.json").then((c) => c.json()));
-
         patches.push(after("getUser", UserStore, (_, ret) => {
             if (ret && users?.has(ret.id)) ret.avatarDecoration = "a_decor"
         }));
@@ -21,6 +19,8 @@ export default {
         patches.push(after("getAvatarDecorationURL", ImageResolver, ([{ userId, avatarDecoration }], _) => {
             if (avatarDecoration === "a_decor") return BASE_URL + `/${userId}.png`
         }));
+
+        users = new Set(await fetch(BASE_URL + "/users.json").then((c) => c.json()));
     },
     onUnload: () => {
         patches.forEach((unpatch) => unpatch());
