9916b1f3c6a2505eec04c2c8a38f5bcb5bde8f43
Jack Matthews <jm5112356@gmail.com>
2023-04-15 19:40:17 -0400

    Switch to Map of user IDs to decoration hash

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
+import { after, before } from "@vendetta/patcher";
 
 const UserStore = findByStoreName("UserStore");
 const ImageResolver = findByProps("getAvatarDecorationURL", "default");
 
 const BASE_URL = "https://decor.fieryflames.dev";
 
-let users: Set<string>;
+let users: Map<string, string>;
 
 let patches = [];
 
 export default {
     onLoad: async () => {
         patches.push(after("getUser", UserStore, (_, ret) => {
-            if (ret && users?.has(ret.id)) ret.avatarDecoration = "a_decor"
+            if (ret && users?.has(ret.id)) ret.avatarDecoration = `decor_${users?.get(ret.id)}`
         }));
 
-        patches.push(after("getAvatarDecorationURL", ImageResolver, ([{ userId, avatarDecoration }], _) => {
-            if (avatarDecoration === "a_decor") return BASE_URL + `/${userId}.png`
+        patches.push(after("getAvatarDecorationURL", ImageResolver, ([{ avatarDecoration }], _) => {
+            if (avatarDecoration?.startsWith("decor")) {
+                const parts = avatarDecoration.split("_");
+                parts.shift();
+                return BASE_URL + `/${parts.join("_")}.png`
+            };
         }));
 
-        users = new Set(await fetch(BASE_URL + "/users.json").then((c) => c.json()));
+        users = new Map(Object.entries(await fetch(BASE_URL + "/users.json").then((c) => c.json())));
     },
     onUnload: () => {
         patches.forEach((unpatch) => unpatch());
